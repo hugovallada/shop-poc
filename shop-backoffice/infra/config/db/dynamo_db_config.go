a42b50a15c37df5501cf9a48992b21bd07225c7d
Hugo <valladahugo@gmail.com>
2024-05-26 14:25:47 -0300

    Separa a criação da tabela

--- shop-backoffice/infra/config/db/dynamo_db_config.go
+++ shop-backoffice/infra/config/db/dynamo_db_config.go
@@ -14,56 +14,17 @@ func ConfigDynamoDB() *dynamodb.DynamoDB {
 
 func ConfigDynamoDBLocal() *dynamodb.DynamoDB {
 	dynamoClient := dynamodb.New(prepareLocalSess())
-	_, err := dynamoClient.DescribeTable(&dynamodb.DescribeTableInput{
-		TableName: aws.String("Products"),
-	})
-	if err != nil {
-		_, err := dynamoClient.CreateTable(&dynamodb.CreateTableInput{
-			TableName: aws.String("Products"),
-			KeySchema: []*dynamodb.KeySchemaElement{
-				{
-					AttributeName: aws.String("id"),
-					KeyType:       aws.String("HASH"),
-				},
-			},
-			AttributeDefinitions: []*dynamodb.AttributeDefinition{
-				{
-					AttributeName: aws.String("id"),
-					AttributeType: aws.String("S"),
-				},
-				{
-					AttributeName: aws.String("name"),
-					AttributeType: aws.String("S"),
-				},
-			},
-			ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
-				ReadCapacityUnits:  aws.Int64(5),
-				WriteCapacityUnits: aws.Int64(5),
-			},
-			GlobalSecondaryIndexes: []*dynamodb.GlobalSecondaryIndex{
-				{
-					IndexName: aws.String("name_gsi"),
-					KeySchema: []*dynamodb.KeySchemaElement{
-						{
-							AttributeName: aws.String("name"),
-							KeyType:       aws.String("HASH"),
-						},
-					},
-					Projection: &dynamodb.Projection{
-						ProjectionType: aws.String("ALL"),
-					},
-					ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
-						ReadCapacityUnits:  aws.Int64(5),
-						WriteCapacityUnits: aws.Int64(5),
-					},
-				},
-			},
-		})
-		if err != nil {
-			slog.Error("Não foi possível configurar a tabela, será necessário criar manualmente", slog.Any("error", err.Error()))
-		}
+	if _, err := dynamoClient.DescribeTable(&dynamodb.DescribeTableInput{TableName: aws.String("Products")}); err != nil {
+		slog.Info("Tabela 'Products' não existe.Tentando criar tabela...")
+		createTableProducts(dynamoClient)
 	}
 	return dynamoClient
+}
+
+func prepareSess() *session.Session {
+	return session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("sa-east-1"),
+	}))
 }
 
 func prepareLocalSess() *session.Session {
@@ -73,8 +34,51 @@ func prepareLocalSess() *session.Session {
 	}))
 }
 
-func prepareSess() *session.Session {
-	return session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("sa-east-1"),
-	}))
+func createTableProducts(dynamoClient *dynamodb.DynamoDB) {
+	_, err := dynamoClient.CreateTable(&dynamodb.CreateTableInput{
+		TableName: aws.String("Products"),
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("id"),
+				KeyType:       aws.String("HASH"),
+			},
+		},
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("id"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("name"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(5),
+			WriteCapacityUnits: aws.Int64(5),
+		},
+		GlobalSecondaryIndexes: []*dynamodb.GlobalSecondaryIndex{
+			{
+				IndexName: aws.String("name_gsi"),
+				KeySchema: []*dynamodb.KeySchemaElement{
+					{
+						AttributeName: aws.String("name"),
+						KeyType:       aws.String("HASH"),
+					},
+				},
+				Projection: &dynamodb.Projection{
+					ProjectionType: aws.String("ALL"),
+				},
+				ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+					ReadCapacityUnits:  aws.Int64(5),
+					WriteCapacityUnits: aws.Int64(5),
+				},
+			},
+		},
+	})
+	if err != nil {
+		slog.Error("Não foi possível configurar a tabela, será necessário criar manualmente", slog.Any("error", err.Error()))
+		return
+	}
+	slog.Info("Tabela 'Products' criada com sucesso.")
 }
